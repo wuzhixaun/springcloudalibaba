2b2ebb52ce065ed8329e506ec4ba5846c59c6e0f
wuzhixuan <jackwu@java.com>
2021-09-09 00:27:34 +0800

    修改配置

--- src/main/java/com/wuzx/springcloudalibaba/conotroller/UserController.java
+++ src/main/java/com/wuzx/springcloudalibaba/conotroller/UserController.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RefreshScope
+@RefreshScope // 可以帮助我们做局部的参数刷新，但是侵入性较强，需要开发阶提前预知可能的刷新点，并且该注解底层是依赖cglib进行代理的
 public class UserController {
 
 
